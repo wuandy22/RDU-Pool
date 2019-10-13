39dd2de46c990a8d271b7826e52774a22897a3aa
Alex Wang <jianuo.wang@duke.edu>
2019-10-12 21:43:54 -0400

    Version 1.0.0

--- server.js
+++ server.js
@@ -66,6 +66,16 @@ app.post('/api/form', (req, res) => {
   })
 })
 
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
